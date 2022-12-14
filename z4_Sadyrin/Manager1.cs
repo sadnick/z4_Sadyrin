330fe489f69add7f9267c9cbdea70aa8ee927322
Николай Садырин <sadyrinnk@mail.ru>
2022-12-14 18:18:12 +0300

    Добавьте файлы проекта.

---
+++ z4_Sadyrin/Manager1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace z4_Sadyrin
+{
+    internal class Manager1
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
